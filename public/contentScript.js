beca8041e87cf302f92851132c42e6c1e94011b3
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2023-05-09 01:52:28 +0530

    code commented

--- public/contentScript.js
+++ public/contentScript.js
@@ -3,7 +3,18 @@
  * @returns {string}
  */
 function getAuthorName() {
-    return location.href?.split("/")[3];
+    // e.g. https://medium.com/@bsalwiczek
+    if(location.href?.split("/")[3]) {
+        return location.href?.split("/")[3];
+    }
+    // e.g. https://lindacaroll.medium.com/
+    else if (location.href?.split("/")?.at(2).split(".")[0]) {
+        return location.href?.split("/")?.at(2).split(".")[0];
+    }
+    else {
+        // e.g. https://medium.com/the-partnered-pen/i-asked-chatgpt-how-to-earn-1000-online-it-was-hilarious-33189ab03f60
+       return '@'+document.querySelector("link[rel='author']")?.getAttribute("href").split("/")?.at(2).split(".")[0];
+    }
     // return document.querySelector("link[id=feedLink]")?.getAttribute("href")?.split("/")?.at(-1);
 }
 
